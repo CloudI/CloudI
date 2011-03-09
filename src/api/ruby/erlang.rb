e31d65f0998ab30b82eeb03a8f4d7800f418cc82
Michael Truog <mjtruog@gmail.com>
2011-03-09 01:56:10 -0800

    Remove the dependency on ruby gem/rake.  Convert the python erlang ei protocol implementation to pure ruby.

---
+++ src/api/ruby/erlang.rb
@@ -0,0 +1,497 @@
+# -*- coding: utf-8; Mode: ruby; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
+# ex: set softtabstop=4 tabstop=4 shiftwidth=4 expandtab fileencoding=utf-8
+#
+# BSD LICENSE
+# 
+# Copyright (c) 2011, Michael Truog <mjtruog at gmail dot com>
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met
+# 
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#     * All advertising materials mentioning features or use of this
+#       software must display the following acknowledgment
+#         This product includes software developed by Michael Truog
+#     * The name of the author may not be used to endorse or promote
+#       products derived from this software without specific prior
+#       written permission
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+# CONTRIBUTORS 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+# DAMAGE.
+#
+
+module Erlang
+
+    class OtpErlangList
+        def initialize(value)
+            @value = value
+        end
+        def to_s
+            arity = @value.length
+            if arity == 0
+                return TAG_NIL_EXT.chr
+            else
+                return (TAG_LIST_EXT.chr + [arity].pack('N') +
+                    @value.map{ |element| term_to_binary_(element)}.join
+                )
+            end
+        end
+    end
+
+    class OtpErlangAtom
+        def initialize(value)
+            @value = value
+        end
+        def to_s
+            if @value.kind_of? Integer
+                return TAG_ATOM_CACHE_REF.chr + @value.chr
+            elsif @value.kind_of? String
+                size = @value.length
+                if size < 256
+                    return TAG_SMALL_ATOM_EXT.chr + size.chr + @value
+                else
+                    return TAG_ATOM_EXT.chr + [size].pack('n') + @value
+                end
+            else
+                raise OutputException, 'unknown atom type', caller
+            end
+        end
+    end
+    
+    class OtpErlangBinary
+        def initialize(value, bits = 8)
+            @value = value
+            @bits = bits # bits in last byte
+        end
+        def to_s
+            size = @value.length
+            if @bits != 8
+                return TAG_BIT_BINARY_EXT.chr + [size].pack('N') +
+                       @bits.chr + @value
+            else
+                return TAG_BINARY_EXT.chr + [size].pack('N') + @value
+            end
+        end
+    end
+    
+    class OtpErlangFunction
+        def initialize(tag, value)
+            @tag = tag
+            @value = value
+        end
+        def to_s
+            return @tag.chr + @value
+        end
+    end
+    
+    class OtpErlangReference
+        def initialize(node, id, creation)
+            @node = node
+            @id = id
+            @creation = creation
+        end
+        def to_s
+            size = @id.length / 4
+            if size > 1
+                return TAG_NEW_REFERENCE_EXT.chr + [size].pack('n') +
+                       @node.to_s + @creation + @id
+            else
+                return TAG_REFERENCE_EXT.chr + @node.to_s + @id + @creation
+            end
+        end
+    end
+    
+    class OtpErlangPort
+        def initialize(node, id, creation)
+            @node = node
+            @id = id
+            @creation = creation
+        end
+        def to_s
+            return TAG_PORT_EXT.chr + @node.to_s + @id + @creation
+        end
+    end
+    
+    class OtpErlangPid
+        def initialize(node, id, serial, creation)
+            @node = node
+            @id = id
+            @serial = serial
+            @creation = creation
+        end
+        def to_s
+            return TAG_PID_EXT.chr + @node.to_s + @id + @serial + @creation
+        end
+    end
+    
+    def binary_to_term(data)
+        if data[0].ord != TAG_VERSION
+            raise ParseException, 'invalid version', caller
+        end
+        result = binary_to_term_(1, data)
+        if result[0] != data.length
+            raise ParseException, 'unparsed data', caller
+        end
+        return result[1]
+    end
+    
+    def term_to_binary(term)
+        return TAG_VERSION.chr + term_to_binary_(term)
+    end
+    
+    private
+    
+    # binary_to_term
+    
+    def binary_to_term_(i, data)
+        tag = data[i].ord
+        i += 1
+        if tag == TAG_NEW_FLOAT_EXT
+            return [i + 8, data[i,8].unpack('G')[0]]
+        elsif tag == TAG_BIT_BINARY_EXT
+            j = data[i,4].unpack('N')[0]
+            i += 4
+            bits = data[i].ord
+            i += 1
+            return [i + j, OtpErlangBinary.new(data[i,j], bits)]
+        elsif tag == TAG_ATOM_CACHE_REF
+            return [i + 1, OtpErlangAtom.new(ord(data[i,1]))]
+        elsif tag == TAG_SMALL_INTEGER_EXT
+            return [i + 1, ord(data[i])]
+        elsif tag == TAG_INTEGER_EXT
+            return [i + 4, data[i,4].unpack('N')[0]]
+        elsif tag == TAG_FLOAT_EXT
+            value = data[i,31].partition(0.chr)[0].to_f
+            return [i + 31, value]
+        elsif tag == TAG_ATOM_EXT
+            j = data[i,2].unpack('n')[0]
+            i += 2
+            return [i + j, OtpErlangAtom.new(data[i,j])]
+        elsif tag == TAG_REFERENCE_EXT or tag == TAG_PORT_EXT
+            result = binary_to_atom(i, data)
+            i = result[0]; node = result[1]
+            id = data[i,4]
+            i += 4
+            creation = data[i]
+            i += 1
+            if tag == TAG_REFERENCE_EXT
+                return [i, OtpErlangReference.new(node, id, creation)]
+            elsif tag == TAG_PORT_EXT
+                return [i, OtpErlangPort.new(node, id, creation)]
+            end
+        elsif tag == TAG_PID_EXT
+            result = binary_to_atom(i, data)
+            i = result[0]; node = result[1]
+            id = data[i,4]
+            i += 4
+            serial = data[i,4]
+            i += 4
+            creation = data[i]
+            i += 1
+            return [i, OtpErlangPid.new(node, id, serial, creation)]
+        elsif tag == TAG_SMALL_TUPLE_EXT or tag == TAG_LARGE_TUPLE_EXT
+            if tag == TAG_SMALL_TUPLE_EXT
+                arity = data[i].ord
+                i += 1
+            elsif tag == TAG_LARGE_TUPLE_EXT
+                arity = data[i,4].unpack('N')[0]
+                i += 4
+            end
+            result = binary_to_term_sequence(i, arity, data)
+            i = result[0]; tmp = result[1]
+            return [i, tmp]
+        elsif tag == TAG_NIL_EXT
+            return [i, OtpErlangList.new([])]
+        elsif tag == TAG_STRING_EXT
+            j = data[i,2].unpack('n')[0]
+            i += 2
+            return [i + j, data[i,j]]
+        elsif tag == TAG_LIST_EXT
+            arity = data[i,4].unpack('N')[0]
+            i += 4
+            result = binary_to_term_sequence(i, arity, data)
+            i = result[0]; tmp = result[1]
+            result = binary_to_term_(i, data)
+            i = result[0]; tail = result[1]
+            if tail.iskind? OtpErlangList == false
+                tmp.append(tail)
+            end
+            return [i, tmp]
+        elsif tag == TAG_BINARY_EXT
+            j = data[i,4].unpack('N')[0]
+            i += 4
+            return [i + j, OtpErlangBinary.new(data[i,j], 8)]
+        elsif tag == TAG_SMALL_BIG_EXT or tag == TAG_LARGE_BIG_EXT
+            if tag == TAG_SMALL_BIG_EXT
+                j = data[i].ord
+                i += 1
+            elsif tag == TAG_LARGE_BIG_EXT
+                j = data[i,4].unpack('N')[0]
+                i += 4
+            end
+            sign = data[i].ord
+            i += 1
+            bignum = 0
+            (0...j).each do |bignum_index|
+                digit = data[i...(j - bignum_index)].ord
+                bignum = bignum * 256 + digit
+            end
+            if sign
+                bignum *= -1
+            end
+            return [i + j, bignum]
+        elsif tag == TAG_NEW_FUN_EXT
+            size = data[i,4].unpack('N')[0]
+            return [i + size, OtpErlangFunction.new(tag, data[i,size])]
+        elsif tag == TAG_EXPORT_EXT
+            old_i = i
+            result = binary_to_atom(i, data)
+            i = result[0]; name_module = result[1]
+            result = binary_to_atom(i, data)
+            i = result[0]; function = result[1]
+            if data[i].ord != TAG_SMALL_INTEGER_EXT
+                raise ParseException, 'invalid small integer tag', caller
+            end
+            i += 1
+            arity = data[i].ord
+            i += 1
+            return [i, OtpErlangFunction.new(tag, data[old_i,i])]
+        elsif tag == TAG_NEW_REFERENCE_EXT
+            j = data[i,2].unpack('n')[0] * 4
+            i += 2
+            result = binary_to_atom(i, data)
+            i = result[0]; node = result[1]
+            creation = data[i]
+            i += 1
+            id = data[i,j]
+            return [i + j, OtpErlangReference.new(node, id, creation)]
+        elsif tag == TAG_SMALL_ATOM_EXT
+            j = data[i,1].ord
+            i += 1
+            return [i + j, OtpErlangAtom.new(data[i,j])]
+        elsif tag == TAG_FUN_EXT
+            old_i = i
+            numfree = data[i,4].unpack('N')[0]
+            i += 4
+            result = binary_to_pid(i, data)
+            i = result[0]; pid = result[1]
+            result = binary_to_atom(i, data)
+            i = result[0]; name_module = result[1]
+            result = binary_to_integer(i, data)
+            i = result[0]; index = result[1]
+            result = binary_to_integer(i, data)
+            i = result[0]; uniq = result[1]
+            result = binary_to_term_sequence(i, numfree, data)
+            i = result[0]; free = result[1]
+            return [i, OtpErlangFunction.new(tag, data[old_i,i])]
+        else
+            raise ParseException, 'invalid tag', caller
+        end
+    end
+    
+    def binary_to_term_sequence(i, arity, data)
+        sequence = []
+        (0...arity).each do |arity_index|
+            result = binary_to_term_(i, data)
+            i = result[0]; element = result[1]
+            sequence.append(element)
+        end
+        return [i, sequence]
+    end
+            
+    def binary_to_integer(i, data)
+        tag = data[i].ord
+        i += 1
+        if tag == TAG_SMALL_INTEGER_EXT
+            return [i + 1, data[i].ord]
+        elsif tag == TAG_INTEGER_EXT
+            return [i + 4, data[i,4].unpack('N')[0]]
+        else
+            raise ParseException, 'invalid integer tag', caller
+        end
+    end
+    
+    def binary_to_pid(i, data)
+        tag = data[i].ord
+        i += 1
+        if tag == TAG_PID_EXT
+            result = binary_to_atom(i, data)
+            i = result[0]; node = result[1]
+            id = data[i,4]
+            i += 4
+            serial = data[i,4]
+            i += 4
+            creation = data[i]
+            i += 1
+            return [i, OtpErlangPid.new(node, id, serial, creation)]
+        else
+            raise ParseException, 'invalid pid tag', caller
+        end
+    end
+    
+    def binary_to_atom(i, data)
+        tag = data[i].ord
+        i += 1
+        if tag == TAG_ATOM_EXT
+            j = data[i,2].unpack('n')[0]
+            i += 2
+            return [i + j, OtpErlangAtom.new(data[i,j])]
+        elsif tag == TAG_ATOM_CACHE_REF
+            return [i + 1, OtpErlangAtom.new(data[i,1].ord)]
+        elsif tag == TAG_SMALL_ATOM_EXT
+            j = data[i,1].ord
+            i += 1
+            return [i + j, OtpErlangAtom.new(data[i,j])]
+        else
+            raise ParseException, 'invalid atom tag', caller
+        end
+    end
+    
+    # term_to_binary
+    
+    def term_to_binary_(term)
+        if term.kind_of? String
+            return string_to_binary(term)
+        elsif term.kind_of? OtpErlangList
+            return term.to_s
+        elsif term.kind_of? Array
+            return tuple_to_binary(term)
+        elsif term.kind_of? Float
+            return float_to_binary(term)
+        elsif term.kind_of? Integer
+            return integer_to_binary(term)
+        elsif term.kind_of? Symbol
+            return OtpErlangAtom.new(term.to_s).to_s
+        elsif term.kind_of? OtpErlangAtom
+            return term.to_s
+        elsif term.kind_of? OtpErlangBinary
+            return term.to_s
+        elsif term.kind_of? OtpErlangFunction
+            return term.to_s
+        elsif term.kind_of? OtpErlangReference
+            return term.to_s
+        elsif term.kind_of? OtpErlangPort
+            return term.to_s
+        elsif term.kind_of? OtpErlangPid
+            return term.to_s
+        else
+            raise OutputException, 'unknown ruby type', caller
+        end
+    end
+    
+    def string_to_binary(term)
+        arity = term.length
+        if arity == 0
+            return TAG_NIL_EXT.chr
+        elsif arity < 65536
+            return TAG_STRING_EXT.chr + [arity].pack('n') + term
+        else
+            return (TAG_LIST_EXT.chr + [arity].pack('N') +
+                term.unpack("C#{term.length}").map{ |c|
+                    TAG_SMALL_INTEGER_EXT.chr + c
+                }.join
+            )
+        end
+    end
+    
+    def tuple_to_binary(term)
+        arity = term.length
+        if arity < 256
+            return (TAG_SMALL_TUPLE_EXT.chr + arity.chr +
+                term.map{ |element| term_to_binary_(element)}.join
+            )
+        else
+            return (TAG_LARGE_TUPLE_EXT.chr + [arity].pack('N') +
+                term.map{ |element| term_to_binary_(element)}.join
+            )
+        end
+    end
+    
+    def integer_to_binary(term)
+        if 0 <= term and term <= 255
+            return TAG_SMALL_INTEGER_EXT.chr + term.chr
+        elsif -2147483648 <= term and term <= 2147483647
+            return TAG_INTEGER_EXT.chr + [term].pack('N')
+        else
+            bignum_to_binary(term)
+        end
+    end
+    
+    def bignum_to_binary(term)
+        bignum = term.abs
+        size = Integer.new(Math.log(bignum) / Math.log(256)) + 1
+        if term < 0
+            sign = 1.chr
+        else
+            sign = 0.chr
+        end
+        l = []
+        (0...size).each do |byte|
+            l.append((bignum & 255).chr)
+            bignum >>= 8
+        end
+        if size < 256
+            return TAG_SMALL_BIG_EXT.chr + size.chr + l.join
+        else
+            return TAG_LARGE_BIG_EXT.chr + [size].pack('N') + l.join
+        end
+    end
+    
+    def float_to_binary(term)
+        return TAG_NEW_FLOAT_EXT.chr + [term].pack('G')
+    end
+
+    # exceptions
+    
+    class ParseException < SyntaxError
+    end
+    
+    class OutputException < TypeError
+    end
+
+    # tag values here http://www.erlang.org/doc/apps/erts/erl_ext_dist.html
+    TAG_VERSION = 131
+    TAG_NEW_FLOAT_EXT = 70
+    TAG_BIT_BINARY_EXT = 77
+    TAG_ATOM_CACHE_REF = 78
+    TAG_SMALL_INTEGER_EXT = 97
+    TAG_INTEGER_EXT = 98
+    TAG_FLOAT_EXT = 99
+    TAG_ATOM_EXT = 100
+    TAG_REFERENCE_EXT = 101
+    TAG_PORT_EXT = 102
+    TAG_PID_EXT = 103
+    TAG_SMALL_TUPLE_EXT = 104
+    TAG_LARGE_TUPLE_EXT = 105
+    TAG_NIL_EXT = 106
+    TAG_STRING_EXT = 107
+    TAG_LIST_EXT = 108
+    TAG_BINARY_EXT = 109
+    TAG_SMALL_BIG_EXT = 110
+    TAG_LARGE_BIG_EXT = 111
+    TAG_NEW_FUN_EXT = 112
+    TAG_EXPORT_EXT = 113
+    TAG_NEW_REFERENCE_EXT = 114
+    TAG_SMALL_ATOM_EXT = 115
+    TAG_FUN_EXT = 117
+
+end
+
