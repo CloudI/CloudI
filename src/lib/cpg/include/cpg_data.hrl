e3ef17689aea548a915cf5d46d003af1dcd99d1e
Michael Truog <mjtruog@gmail.com>
2012-07-19 16:05:18 -0700

    Add cpg, uuid, and trie applications instead of using modules within the CloudI application (this includes making cpg more abstract based on the list_pg source code).

--- src/lib/cpg/include/cpg_data.hrl
+++ src/lib/cpg/include/cpg_data.hrl
@@ -1,7 +1,7 @@
 %%% -*- coding: utf-8; Mode: erlang; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
 %%% ex: set softtabstop=4 tabstop=4 shiftwidth=4 expandtab fileencoding=utf-8:
 
--record(list_pg_data,
+-record(cpg_data,
     {
         local_count = 0,
         local = [],
@@ -9,7 +9,7 @@
         remote = []
     }).
 
--record(list_pg_data_pid,
+-record(cpg_data_pid,
     {
         pid,
         monitor
