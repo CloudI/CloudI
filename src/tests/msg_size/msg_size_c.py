632856d2e0d783a67a8acbd9a6bc5d12d61ccb20
Michael Truog <mjtruog@gmail.com>
2012-08-14 23:16:14 -0700

    Add missing file.

---
+++ src/tests/msg_size/msg_size_c.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; Mode: python; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
+# ex: set softtabstop=4 tabstop=4 shiftwidth=4 expandtab fileencoding=utf-8:
+#
+# BSD LICENSE
+# 
+# Copyright (c) 2012, Michael Truog <mjtruog at gmail dot com>
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#     * All advertising materials mentioning features or use of this
+#       software must display the following acknowledgment:
+#         This product includes software developed by Michael Truog
+#     * The name of the author may not be used to endorse or promote
+#       products derived from this software without specific prior
+#       written permission
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+# DAMAGE.
+#
+
+import sys, os
+sys.path.append(
+    os.path.sep.join(
+        os.path.dirname(os.path.abspath(__file__))
+               .split(os.path.sep)[:-2] + ['api', 'python']
+    )
+)
+
+import threading, struct
+from cloudi_c import API
+
+_DESTINATION = '/tests/msg_size/erlang'
+
+class _Task(threading.Thread):
+    def __init__(self, thread_index):
+        threading.Thread.__init__(self)
+        self.__api = API(thread_index)
+
+    def run(self):
+        self.__api.subscribe('python_c', self.request)
+
+        result = self.__api.poll()
+        print 'exited thread:', result
+
+    def request(self, command, name, pattern, requestInfo, request,
+                timeout, priority, transId, pid):
+        i = struct.unpack('=I', request[:4])[0]
+        if i == 4294967295:
+            i = 0
+        else:
+            i += 1
+        request = struct.pack('=I', i) + request[4:]
+        print 'forward #%d python_c to %s (with timeout %d ms)' % (
+            i, _DESTINATION, timeout,
+        )
+        self.__api.forward_(command, _DESTINATION, requestInfo, request,
+                            timeout, priority, transId, pid)
+
+if __name__ == '__main__':
+    thread_count = API.thread_count()
+    assert thread_count >= 1
+    
+    threads = [_Task(i) for i in range(thread_count)]
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+
