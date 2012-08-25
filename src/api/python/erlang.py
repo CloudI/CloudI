ca0410e185694a119798caaa66e6297de6c3620f
Michael Truog <mjtruog@gmail.com>
2012-08-24 22:33:21 -0700

    Make python erlang binary term tests executable.

--- src/api/python/erlang.py
+++ src/api/python/erlang.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8; Mode: python; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
 # ex: set softtabstop=4 tabstop=4 shiftwidth=4 expandtab fileencoding=utf-8:
 #
