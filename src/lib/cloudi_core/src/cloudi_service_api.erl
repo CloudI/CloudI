e2f1ed5f6fb6393bbd866e3d1c5f1305b5e80e19
Michael Truog <mjtruog@gmail.com>
2013-06-14 14:28:49 -0700

    Add cloudi_service_api module documentation. #18

--- src/lib/cloudi_core/src/cloudi_service_api.erl
+++ src/lib/cloudi_core/src/cloudi_service_api.erl
@@ -129,6 +129,18 @@
 %%% External interface functions
 %%%------------------------------------------------------------------------
 
+%%-------------------------------------------------------------------------
+%% @doc
+%% ===Add ACL entries.===
+%% Add more ACL entries to be later used when starting services. An ACL
+%% entry is an Erlang atom() -> list(atom() | string()) relationship which
+%% provides a logical grouping of service name patterns
+%% (e.g., {api, ["/cloudi/api/"]}). When providing a service name pattern
+%% for an ACL entry, a non-pattern will be assumed to be a prefix
+%% (i.e., "/cloudi/api/" == "/cloudi/api/*").
+%% @end
+%%-------------------------------------------------------------------------
+
 -spec acl_add(L :: list({atom(), list(acl())}), 
               Timeout :: pos_integer()) ->
     ok.
@@ -136,6 +148,15 @@
 acl_add(L, Timeout)
     when is_integer(Timeout), Timeout > ?TIMEOUT_DELTA ->
     cloudi_configurator:acl_add(L, Timeout).
+
+%%-------------------------------------------------------------------------
+%% @doc
+%% ===Remove ACL entries.===
+%% Remove ACL entries that are no longer needed. Running services will
+%% retain their configuration, so this impacts services that are started
+%% in the future.
+%% @end
+%%-------------------------------------------------------------------------
 
 -spec acl_remove(L :: list(atom()),
                  Timeout :: pos_integer()) ->
@@ -145,6 +166,14 @@ acl_remove(L, Timeout)
     when is_integer(Timeout), Timeout > ?TIMEOUT_DELTA ->
     cloudi_configurator:acl_remove(L, Timeout).
 
+%%-------------------------------------------------------------------------
+%% @doc
+%% ===Add service instances.===
+%% Provide service configuration using the same syntax found in the
+%% configuration file (i.e., /usr/local/etc/cloudi/cloudi.conf).
+%% @end
+%%-------------------------------------------------------------------------
+
 -spec services_add(L :: list(#internal{} | #external{}),
                    Timeout :: pos_integer()) ->
     ok.
@@ -152,6 +181,17 @@ acl_remove(L, Timeout)
 services_add(L, Timeout)
     when is_integer(Timeout), Timeout > ?TIMEOUT_DELTA ->
     cloudi_configurator:services_add(L, Timeout).
+
+%%-------------------------------------------------------------------------
+%% @doc
+%% ===Remove service instances.===
+%% Provide the Service UUIDs for the services that should be stopped.
+%% The Service UUID is shown in the output of services/1. When the
+%% service is stopped, its running instance is removed from CloudI, but
+%% does not impact any other running instances (even if they are the same
+%% service module or binary).
+%% @end
+%%-------------------------------------------------------------------------
 
 -spec services_remove(L :: list(<<_:128>>),
                       Timeout :: pos_integer()) ->
@@ -161,6 +201,19 @@ services_remove(L, Timeout)
     when is_integer(Timeout), Timeout > ?TIMEOUT_DELTA ->
     cloudi_configurator:services_remove(L, Timeout).
 
+%%-------------------------------------------------------------------------
+%% @doc
+%% ===Restart service instances.===
+%% Provide the Service UUIDs for the services that should be restarted.
+%% The Service UUID is shown in the output of services/1. When the service
+%% is restarted, the old instance is stopped and a new instance is started.
+%% During the restart delay, it is possible to lose queued service
+%% requests and received asynchronous responses. Keeping the state
+%% separate between the service instances is important to prevent failures
+%% within the new instance.
+%% @end
+%%-------------------------------------------------------------------------
+
 -spec services_restart(L :: list(<<_:128>>), 
                        Timeout :: pos_integer()) ->
     ok.
@@ -169,12 +222,26 @@ services_restart(L, Timeout)
     when is_integer(Timeout), Timeout > ?TIMEOUT_DELTA ->
     cloudi_configurator:services_restart(L, Timeout).
 
+%%-------------------------------------------------------------------------
+%% @doc
+%% ===List all service instances with each service's UUID.===
+%% @end
+%%-------------------------------------------------------------------------
+
 -spec services(Timeout :: pos_integer()) ->
     ok.
 
 services(Timeout)
     when is_integer(Timeout), Timeout > ?TIMEOUT_DELTA ->
     cloudi_configurator:services(Timeout).
+
+%%-------------------------------------------------------------------------
+%% @doc
+%% ===Add CloudI nodes.===
+%% Explicitly add a CloudI node name, so that services between all other
+%% CloudI nodes and the added nodes can send each other service requests.
+%% @end
+%%-------------------------------------------------------------------------
 
 -spec nodes_add(L :: list(atom()),
                 Timeout :: pos_integer()) ->
@@ -184,6 +251,12 @@ nodes_add(L, Timeout)
     when is_integer(Timeout), Timeout > ?TIMEOUT_DELTA ->
     cloudi_configurator:nodes_add(L, Timeout).
 
+%%-------------------------------------------------------------------------
+%% @doc
+%% ===Explicitly remove CloudI nodes.===
+%% @end
+%%-------------------------------------------------------------------------
+
 -spec nodes_remove(L :: list(atom()),
                    Timeout :: pos_integer()) ->
     ok.
@@ -192,12 +265,24 @@ nodes_remove(L, Timeout)
     when is_integer(Timeout), Timeout > ?TIMEOUT_DELTA ->
     cloudi_configurator:nodes_remove(L, Timeout).
 
+%%-------------------------------------------------------------------------
+%% @doc
+%% ===List all the CloudI nodes known to be connected.===
+%% @end
+%%-------------------------------------------------------------------------
+
 -spec nodes_alive(Timeout :: pos_integer()) ->
     list(atom()).
 
 nodes_alive(Timeout)
     when is_integer(Timeout), Timeout > ?TIMEOUT_DELTA ->
     cloudi_nodes:alive(Timeout).
+
+%%-------------------------------------------------------------------------
+%% @doc
+%% ===List all the CloudI nodes that are disconnected but expected to reconnect.===
+%% @end
+%%-------------------------------------------------------------------------
 
 -spec nodes_dead(Timeout :: pos_integer()) ->
     list(atom()).
@@ -206,12 +291,26 @@ nodes_dead(Timeout)
     when is_integer(Timeout), Timeout > ?TIMEOUT_DELTA ->
     cloudi_nodes:dead(Timeout).
 
+%%-------------------------------------------------------------------------
+%% @doc
+%% ===List both the connected and disconnected CloudI nodes.===
+%% @end
+%%-------------------------------------------------------------------------
+
 -spec nodes(Timeout :: pos_integer()) ->
     list(atom()).
 
 nodes(Timeout)
     when is_integer(Timeout), Timeout > ?TIMEOUT_DELTA ->
     cloudi_nodes:nodes(Timeout).
+
+%%-------------------------------------------------------------------------
+%% @doc
+%% ===Modify the current loglevel.===
+%% CloudI uses asynchronous logging with flow control (backpressure
+%% handling) to prevent misbehaving services from causing instability.
+%% @end
+%%-------------------------------------------------------------------------
 
 -spec loglevel_set(Level :: loglevel(),
                    Timeout :: pos_integer()) ->
@@ -221,6 +320,14 @@ loglevel_set(Level, Timeout)
     when is_integer(Timeout), Timeout > ?TIMEOUT_DELTA ->
     cloudi_logger:change_loglevel(Level).
 
+%%-------------------------------------------------------------------------
+%% @doc
+%% ===Redirect the log output.===
+%% Redirect all local log output to a remote CloudI node.
+%% Use 'undefined' as the node name to log locally.
+%% @end
+%%-------------------------------------------------------------------------
+
 -spec log_redirect(Node :: atom(),
                    Timeout :: pos_integer()) ->
     ok.
@@ -228,6 +335,14 @@ loglevel_set(Level, Timeout)
 log_redirect(Node, Timeout)
     when is_integer(Timeout), Timeout > ?TIMEOUT_DELTA ->
     cloudi_nodes:logger_redirect(Node).
+
+%%-------------------------------------------------------------------------
+%% @doc
+%% ===Add a directory to the CloudI Erlang VM code server's search paths.===
+%% The path is always appended to the list of search paths (you should not
+%% need to rely on search path order because of unique naming).
+%% @end
+%%-------------------------------------------------------------------------
 
 -spec code_path_add(Dir :: file:filename(),
                     Timeout :: pos_integer()) ->
@@ -237,6 +352,14 @@ code_path_add(Dir, Timeout)
     when is_integer(Timeout), Timeout > ?TIMEOUT_DELTA ->
     code:add_pathz(Dir).
 
+%%-------------------------------------------------------------------------
+%% @doc
+%% ===Remove a directory from the CloudI Erlang VM code server's search paths.===
+%% This doesn't impact any running services, only services that will be
+%% started in the future. 
+%% @end
+%%-------------------------------------------------------------------------
+
 -spec code_path_remove(Dir :: file:filename(),
                        Timeout :: pos_integer()) ->
     ok.
@@ -244,6 +367,13 @@ code_path_add(Dir, Timeout)
 code_path_remove(Dir, Timeout)
     when is_integer(Timeout), Timeout > ?TIMEOUT_DELTA ->
     code:del_path(Dir).
+
+%%-------------------------------------------------------------------------
+%% @doc
+%% ===List all the CloudI Erlang VM code server search paths.===
+%% The order is the same order the directories are searched.
+%% @end
+%%-------------------------------------------------------------------------
 
 -spec code_path(Timeout :: pos_integer()) ->
     list(file:filename()).
