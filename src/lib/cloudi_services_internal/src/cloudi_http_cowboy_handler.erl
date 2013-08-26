f02ed6660778c128bed84f83159cd16100cffc44
Michael Truog <mjtruog@gmail.com>
2013-08-26 13:03:58 -0700

    Add the cloudi_ip_address module for fixed width string representations.

--- src/lib/cloudi_services_internal/src/cloudi_http_cowboy_handler.erl
+++ src/lib/cloudi_services_internal/src/cloudi_http_cowboy_handler.erl
@@ -726,20 +726,12 @@ service_name_incoming_merge(undefined, undefined, PathRaw) ->
 service_name_incoming_merge(undefined, HostRaw, PathRaw) ->
     erlang:binary_to_list(<<HostRaw/binary, PathRaw/binary>>);
 service_name_incoming_merge({ClientIpAddr, _ClientPort}, undefined, PathRaw) ->
-    ip_address_string(ClientIpAddr) ++
+    cloudi_ip_address:to_string(ClientIpAddr) ++
     erlang:binary_to_list(PathRaw);
 service_name_incoming_merge({ClientIpAddr, _ClientPort}, HostRaw, PathRaw) ->
-    ip_address_string(ClientIpAddr) ++
+    cloudi_ip_address:to_string(ClientIpAddr) ++
     erlang:binary_to_list(<<$/, HostRaw/binary, PathRaw/binary>>).
     
-ip_address_string({B1, B2, B3, B4}) ->
-    cloudi_string:format_to_list("~3..0b.~3..0b.~3..0b.~3..0b",
-                                 [B1, B2, B3, B4]);
-ip_address_string({S1, S2, S3, S4, S5, S6, S7, S8}) ->
-    cloudi_string:format_to_list("~4.16.0b:~4.16.0b:~4.16.0b:~4.16.0b:"
-                                 "~4.16.0b:~4.16.0b:~4.16.0b:~4.16.0b",
-                                 [S1, S2, S3, S4, S5, S6, S7, S8]).
-
 process_queue(Req,
               #cowboy_state{websocket_state =
                   #websocket_state{
