a79155ee76daca8b1ac7c754685d2c0aaa39511a
Michael Truog <mjtruog@gmail.com>
2014-09-08 15:29:47 -0700

    Increase cloudi_service_filesystem CT test timeout for Travis build.

--- src/lib/cloudi_service_filesystem/test_ct/cloudi_service_filesystem_SUITE.erl
+++ src/lib/cloudi_service_filesystem/test_ct/cloudi_service_filesystem_SUITE.erl
@@ -78,9 +78,9 @@
 -define(WRITABLE_DIRECTORY, "/tmp/").
 -define(WRITABLE_FILENAME, "cloudi_service_filesystem_test.txt").
 -define(WRITABLE_FILEPATH, ?WRITABLE_DIRECTORY ?WRITABLE_FILENAME).
--define(TIMEOUT, 120000).
--define(REFRESH, 300). % ((?TIMEOUT * 2.5) div 1000)
--define(REFRESH_STRING, "300").
+-define(TIMEOUT, 240000).
+-define(REFRESH, 600). % ((?TIMEOUT * 2.5) div 1000)
+-define(REFRESH_STRING, "600").
 
 %%%------------------------------------------------------------------------
 %%% Callback functions from CT
