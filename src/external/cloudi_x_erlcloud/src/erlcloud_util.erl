e29b7ae5f92baf9875dd10d1e4bcfdb50cd913fa
Michael Truog <mjtruog@gmail.com>
2014-10-31 15:30:30 -0700

    Remove erlcloud =< R15 compatibility due to compiler warnings.

--- src/external/cloudi_x_erlcloud/src/erlcloud_util.erl
+++ src/external/cloudi_x_erlcloud/src/erlcloud_util.erl
@@ -2,43 +2,15 @@
 -export([sha_mac/2, sha256_mac/2,
          md5/1, sha256/1]).
 
--compile({nowarn_deprecated_function,
-          [{crypto, md5, 1}]}).
--compile(nowarn_deprecated_function). % does not work in R16B03-1
-
 sha_mac(K, S) ->
-    try
-        crypto:hmac(sha, K, S)
-    catch
-        error:undef ->
-            R0 = crypto:hmac_init(sha, K),
-            R1 = crypto:hmac_update(R0, S),
-            crypto:hmac_final(R1)
-    end.
+    crypto:hmac(sha, K, S).
         
 sha256_mac(K, S) ->
-    try
-        crypto:hmac(sha256, K, S)
-    catch
-        error:undef ->
-            R0 = crypto:hmac_init(sha256, K),
-            R1 = crypto:hmac_update(R0, S),
-            crypto:hmac_final(R1)
-    end.
+    crypto:hmac(sha256, K, S).
 
 sha256(V) ->
-    try
-        crypto:hash(sha256, V)
-    catch
-        _:_ ->
-            crypto:sha256(V)
-    end.
+    crypto:hash(sha256, V).
 
 md5(V) ->
-    try
-        crypto:hash(md5, V)
-    catch
-        _:_ ->
-            crypto:md5(V)
-    end.
+    crypto:hash(md5, V).
      
