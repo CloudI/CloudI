0776f614884a7772d99a05da3649e8d3f82256c2
Michael Truog <mjtruog@gmail.com>
2014-11-28 19:33:27 -0800

    Fix 1.3.3 bug with count_process_dynamic creating service processes that have a termination timeout which is not set internally, causing CloudI to crash when the new service processes attempt to terminate.

--- src/lib/cloudi_core/src/cloudi_core_i_services_monitor.erl
+++ src/lib/cloudi_core/src/cloudi_core_i_services_monitor.erl
@@ -231,34 +231,19 @@ handle_call({monitor, M, F, A, ProcessIndex, CountProcess, CountThread,
     case erlang:apply(M, F, [ProcessIndex, CountProcess | A]) of
         {ok, Pid} when is_pid(Pid) ->
             Pids = [Pid],
-            NewServices =
-                cloudi_x_key2value:store(ServiceId, Pid,
-                    #service{service_m = M,
-                             service_f = F,
-                             service_a = A,
-                             process_index = ProcessIndex,
-                             count_process = CountProcess,
-                             count_thread = CountThread,
-                             pids = Pids,
-                             monitor = erlang:monitor(process, Pid),
-                             timeout_term = TimeoutTerm,
-                             max_r = MaxR,
-                             max_t = MaxT}, Services),
+            NewServices = cloudi_x_key2value:store(ServiceId, Pid,
+                new_service_process(M, F, A,
+                                    ProcessIndex, CountProcess, CountThread,
+                                    Pids, erlang:monitor(process, Pid),
+                                    TimeoutTerm, MaxR, MaxT), Services),
             {reply, {ok, Pids}, State#state{services = NewServices}};
         {ok, [Pid | _] = Pids} when is_pid(Pid) ->
             NewServices = lists:foldl(fun(P, D) ->
                 cloudi_x_key2value:store(ServiceId, P,
-                    #service{service_m = M,
-                             service_f = F,
-                             service_a = A,
-                             process_index = ProcessIndex,
-                             count_process = CountProcess,
-                             count_thread = CountThread,
-                             pids = Pids,
-                             monitor = erlang:monitor(process, P),
-                             timeout_term = TimeoutTerm,
-                             max_r = MaxR,
-                             max_t = MaxT}, D)
+                    new_service_process(M, F, A,
+                                        ProcessIndex, CountProcess, CountThread,
+                                        Pids, erlang:monitor(process, P),
+                                        TimeoutTerm, MaxR, MaxT), D)
             end, Services, Pids),
             {reply, {ok, Pids}, State#state{services = NewServices}};
         {error, _} = Error ->
@@ -742,6 +727,7 @@ pids_increase_loop(Count, ProcessIndex,
                             service_a = A,
                             count_process = CountProcess,
                             count_thread = CountThread,
+                            timeout_term = TimeoutTerm,
                             max_r = MaxR,
                             max_t = MaxT} = Service, ServiceId, Services) ->
     NewServices = case erlang:apply(M, F, [ProcessIndex, CountProcess | A]) of
@@ -750,16 +736,10 @@ pids_increase_loop(Count, ProcessIndex,
                       [cloudi_x_uuid:uuid_to_string(ServiceId), Pid]),
             Pids = [Pid],
             NextServices = cloudi_x_key2value:store(ServiceId, Pid,
-                #service{service_m = M,
-                         service_f = F,
-                         service_a = A,
-                         process_index = ProcessIndex,
-                         count_process = CountProcess,
-                         count_thread = CountThread,
-                         pids = Pids,
-                         monitor = erlang:monitor(process, Pid),
-                         max_r = MaxR,
-                         max_t = MaxT}, Services),
+                new_service_process(M, F, A,
+                                    ProcessIndex, CountProcess, CountThread,
+                                    Pids, erlang:monitor(process, Pid),
+                                    TimeoutTerm, MaxR, MaxT), Services),
             ok = initialize(Pids),
             NextServices;
         {ok, [Pid | _] = Pids} when is_pid(Pid) ->
@@ -767,16 +747,10 @@ pids_increase_loop(Count, ProcessIndex,
                       [cloudi_x_uuid:uuid_to_string(ServiceId), Pids]),
             NextServices = lists:foldl(fun(P, D) ->
                 cloudi_x_key2value:store(ServiceId, P,
-                    #service{service_m = M,
-                             service_f = F,
-                             service_a = A,
-                             process_index = ProcessIndex,
-                             count_process = CountProcess,
-                             count_thread = CountThread,
-                             pids = Pids,
-                             monitor = erlang:monitor(process, P),
-                             max_r = MaxR,
-                             max_t = MaxT}, D)
+                    new_service_process(M, F, A,
+                                        ProcessIndex, CountProcess, CountThread,
+                                        Pids, erlang:monitor(process, P),
+                                        TimeoutTerm, MaxR, MaxT), D)
             end, Services, Pids),
             ok = initialize(Pids),
             NextServices;
@@ -853,4 +827,18 @@ terminate_service(ServiceId, Pids, Reason,
         cloudi_x_key2value:erase(ServiceId, P, D)
     end, Services, Pids),
     NewServices.
+
+new_service_process(M, F, A, ProcessIndex, CountProcess, CountThread,
+                    Pids, MonitorRef, TimeoutTerm, MaxR, MaxT) ->
+    #service{service_m = M,
+             service_f = F,
+             service_a = A,
+             process_index = ProcessIndex,
+             count_process = CountProcess,
+             count_thread = CountThread,
+             pids = Pids,
+             monitor = MonitorRef,
+             timeout_term = TimeoutTerm,
+             max_r = MaxR,
+             max_t = MaxT}.
 
