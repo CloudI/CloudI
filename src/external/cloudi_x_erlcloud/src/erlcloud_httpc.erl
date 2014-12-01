8406efa020cb024245bfb33a94bf4288889787cf
Michael Truog <mjtruog@gmail.com>
2014-12-01 12:24:44 -0800

    Make erlcloud use httpc instead of lhttpc to keep the errors returned consistent.

--- src/external/cloudi_x_erlcloud/src/erlcloud_httpc.erl
+++ src/external/cloudi_x_erlcloud/src/erlcloud_httpc.erl
@@ -11,5 +11,24 @@
 
 -export([request/6]).
 
-request(URL, Method, Hdrs, Body, Timeout, _Config) ->
+request(URL, Method, Hdrs, Body, Timeout, Config) ->
+    case application:get_env(erlcloud, http_client) of
+        {ok, lhttpc} ->
+            request_lhttpc(URL, Method, Hdrs, Body, Timeout, Config);
+        {ok, httpc} ->
+            request_httpc(URL, Method, Hdrs, Body, Timeout, Config)
+    end.
+
+request_lhttpc(URL, Method, Hdrs, Body, Timeout, _Config) ->
     lhttpc:request(URL, Method, Hdrs, Body, Timeout, []).
+
+request_httpc(URL, Method, Hdrs, <<>>, Timeout, _Config) ->
+    HdrsStr = [{binary_to_list(K), binary_to_list(V)} || {K, V} <- Hdrs],
+    httpc:request(Method, {URL, HdrsStr},
+                  [{timeout, Timeout}], []);
+request_httpc(URL, Method, Hdrs, Body, Timeout, _Config) ->
+    {value,
+     {_, ContentType}, HdrsRest} = lists:keytake(<<"content-type">>, 1, Hdrs),
+    HdrsStr = [{binary_to_list(K), binary_to_list(V)} || {K, V} <- HdrsRest],
+    httpc:request(Method, {URL, HdrsStr, binary_to_list(ContentType), Body},
+                  [{timeout, Timeout}], []).
