020aae5c820ef569bb1b57d6f2c0577a55780eb9
Michael Truog <mjtruog@gmail.com>
2014-12-12 13:05:20 -0800

    Add all metrics to tsung_data.py script.

--- src/tests/http_req/loadtest/tsung_data.py
+++ src/tests/http_req/loadtest/tsung_data.py
@@ -40,43 +40,113 @@
 #
 
 import os, csv
+from optparse import OptionParser
 
-def print_latency(file_path):
+def print_metrics(file_path, host_name):
     initialize_min = 1e6
     initialize_max = 0
-    values = {
-        # can add 'tr_XXX's below (min min, max mean, max max)
+    values_min_mean_max = {
+        # TRANSACTION LATENCY SUMMARY
+        # ('tr_XXX' can be added below)
         'page':    (initialize_min, initialize_max, initialize_max),
         #'request': (initialize_min, initialize_max, initialize_max),
         #'connect': (initialize_min, initialize_max, initialize_max),
     }
-    f = open(file_path, 'rb')
+    values_count_per_sec_max = {
+        # MAX TRANSACTIONS PER SECOND
+        'page': (initialize_max, ),
+    }
+    values_min_max_diff = {}
+    values_max = {}
+    if host_name is not None:
+        # MEMORY CONSUMPTION
+        values_min_max_diff['{freemem,"os_mon@%s"}' % host_name] = (
+            initialize_min, initialize_max,
+        )
+        # CPU CONSUMPTION
+        values_max['{cpu,"os_mon@%s"}' % host_name] = (
+            initialize_max,
+        )
+        # LOAD AVERAGE
+        values_max['{load,"os_mon@%s"}' % host_name] = (
+            initialize_max,
+        )
+    f = open(file_path, 'r')
     lines = csv.reader(f, delimiter=' ')
     for line in lines:
         if len(line) != 9:
             continue
         name = line[1]
-        value = values.get(name)
-        if value is not None:
-            values[name] = (
-                min(float(line[6]), value[0]), # min min
-                max(float(line[3]), value[1]), # max (10 second) mean
-                max(float(line[5]), value[2]), # max max
+        value_min_mean_max = values_min_mean_max.get(name)
+        if value_min_mean_max is not None:
+            values_min_mean_max[name] = (
+                min(float(line[6]), value_min_mean_max[0]), # min min
+                max(float(line[3]), value_min_mean_max[1]), # max (10 sec) mean
+                max(float(line[5]), value_min_mean_max[2]), # max max
+            )
+        value_count_per_sec_max = values_count_per_sec_max.get(name)
+        if value_count_per_sec_max is not None:
+            values_count_per_sec_max[name] = (
+                max(int(line[2]) / 10.0, value_count_per_sec_max[0]),
+            )
+        value_min_max_diff = values_min_max_diff.get(name)
+        if value_min_max_diff is not None:
+            values_min_max_diff[name] = (
+                min(float(line[6]), value_min_max_diff[0]), # min min
+                max(float(line[5]), value_min_max_diff[1]), # max max
+            )
+        value_max = values_max.get(name)
+        if value_max is not None:
+            values_max[name] = (
+                max(float(line[5]), value_max[0]), # max max
             )
     f.close()
-    print('     name\tmin_min    \tmax_mean    \tmax_max')
-    for name, value in values.items():
-        (min_min, max_mean, max_max) = value
+    print('\t\tmin_min    \tmax_mean    \tmax_max\t   ' +
+          '(trans latency)')
+    for name, value_min_mean_max in values_min_mean_max.items():
+        (min_min, max_mean, max_max) = value_min_mean_max
         print('%8s:\t%f\t%f\t%f' % (name, min_min, max_mean, max_max))
+    print('\t\ttransactions_per_second_max\t\t   ' +
+          '(trans throughput)')
+    for name, value_count_per_sec_max in values_count_per_sec_max.items():
+        (count_per_sec_max, ) = value_count_per_sec_max
+        print('%8s:\t%f' % (name, count_per_sec_max))
+    if host_name is not None:
+        print('\t\ttotal_megabytes\t\t\t\t   ' +
+              '(memory)')
+        (min_min,
+         max_max) = values_min_max_diff['{freemem,"os_mon@%s"}' % host_name]
+        print('%8s:\t%f' % ('memory', max_max - min_min))
+        print('\t\tmax_max\t\t\t\t\t   ' +
+              '(processor)')
+        (max_max, ) = values_max['{cpu,"os_mon@%s"}' % host_name]
+        print('%8s:\t%f %%' % ('cpu', max_max))
+        (max_max, ) = values_max['{load,"os_mon@%s"}' % host_name]
+        print('%8s:\t%f' % ('load', max_max))
 
 if __name__ == '__main__':
-    file_name = 'tsung.log'
+    default_file_name = 'tsung.log'
+    default_host_name = None
+    parser = OptionParser()
+    parser.add_option(
+        '-n', '--file-name', dest='file_name',
+        help='Tsung output log',
+        metavar='FILENAME', default=default_file_name, type='string',
+    )
+    parser.add_option(
+        '-s', '--server-name', dest='host_name',
+        help='Monitored hostname (of the loadtest server)',
+        metavar='HOSTNAME', default=default_host_name, type='string',
+    )
+    (options, args) = parser.parse_args()
+    file_name = options.file_name
+    host_name = options.host_name
     if os.access(file_name, os.R_OK):
-        print_latency(file_name)
+        print_metrics(file_name, host_name)
     else:
         for root, dirs, files in os.walk('.'):
             if file_name in files:
                 file_path = os.path.sep.join([root, file_name])
                 print('%s:' % file_path)
-                print_latency(file_path)
+                print_metrics(file_path, host_name)
 
