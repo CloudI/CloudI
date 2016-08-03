cdd92a1b9a7da2b2a675176d5fa29bd1448d3f82
Michael Truog <mjtruog@gmail.com>
2016-08-03 14:18:49 -0700

    Add Java CloudI API timestamp accessor for a microseconds integer value.

--- src/api/java/org/cloudi/API.java
+++ src/api/java/org/cloudi/API.java
@@ -1818,18 +1818,25 @@ public class API
             return toTimestampString(true);
         }
 
+        public long toTimestampMicroSeconds()
+        {
+            // v1 UUID is limited to 60 bit time value
+            // (overflows 60 bits after 5236-03-31 21:21:00)
+            return (((((long) (this.id[6] & 0x0f)) << 56) |
+                     (((long) (this.id[7] & 0xff)) << 48) |
+                     (((long) (this.id[4] & 0xff)) << 40) |
+                     (((long) (this.id[5] & 0xff)) << 32) |
+                     (((long) (this.id[0] & 0xff)) << 24) |
+                     (((long) (this.id[1] & 0xff)) << 16) |
+                     (((long) (this.id[2] & 0xff)) <<  8) |
+                     ((long) (this.id[3] & 0xff))) -
+                    0x01b21dd213814000L) / 10;
+        }
+
         private String toTimestampString(final boolean SQL)
         {
             // microsecond resolution (ISO8601 datetime in UTC)
-            final long micro = (((((long) (this.id[6] & 0x0f)) << 56) |
-                                 (((long) (this.id[7] & 0xff)) << 48) |
-                                 (((long) (this.id[4] & 0xff)) << 40) |
-                                 (((long) (this.id[5] & 0xff)) << 32) |
-                                 (((long) (this.id[0] & 0xff)) << 24) |
-                                 (((long) (this.id[1] & 0xff)) << 16) |
-                                 (((long) (this.id[2] & 0xff)) <<  8) |
-                                 ((long) (this.id[3] & 0xff))) -
-                                0x01b21dd213814000L) / 10;
+            final long micro = toTimestampMicroSeconds();
             final Date date = new Date(micro / 1000);
             final String str =
                 (new SimpleDateFormat("yyyy-MM-ddHH:mm:ss.SSS")).format(date);
