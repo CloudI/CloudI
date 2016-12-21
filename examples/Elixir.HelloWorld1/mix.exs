8a96462b93fa87e9a06bd07feb11f7c1d5f6ac2d
Michael Truog <mjtruog@gmail.com>
2016-12-20 19:23:42 -0800

    Fix Elixir indentation to be 2 spaces.

--- examples/Elixir.HelloWorld1/mix.exs
+++ examples/Elixir.HelloWorld1/mix.exs
@@ -1,31 +1,31 @@
 defmodule HelloWorld1.Mixfile do
-    use Mix.Project
+  use Mix.Project
 
-    def project do
-        [app: :'Elixir.HelloWorld1',
-         version: "1.5.4",
-         elixirc_paths: ["lib/",
-                         "/usr/local/lib/cloudi-1.5.4/lib/cloudi_core-1.5.4/include/"],
-         deps: deps]
-    end
+  def project do
+    [app: :'Elixir.HelloWorld1',
+     version: "1.5.4",
+     elixirc_paths: ["lib/",
+                     "/usr/local/lib/cloudi-1.5.4/lib/cloudi_core-1.5.4/include/"],
+     deps: deps]
+  end
 
-    # Configuration for the OTP application
-    #
-    # Type `mix help compile.app` for more information
-    def application do
-        [applications: []]
-    end
+  # Configuration for the OTP application
+  #
+  # Type `mix help compile.app` for more information
+  def application do
+    [applications: []]
+  end
 
-    # Dependencies can be hex.pm packages:
-    #
-    #   {:mydep, "~> 0.3.0"}
-    #
-    # Or git/path repositories:
-    #
-    #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1"}
-    #
-    # Type `mix help deps` for more examples and options
-    defp deps do
-        []
-    end
+  # Dependencies can be hex.pm packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1"}
+  #
+  # Type `mix help deps` for more examples and options
+  defp deps do
+    []
+  end
 end
