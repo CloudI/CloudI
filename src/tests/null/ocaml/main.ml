3b4f1685053d839d31bebc0c74490b220b35615f
Michael Truog <mjtruog@gmail.com>
2017-02-21 18:23:48 -0800

    Add the null integration test in OCaml.

---
+++ src/tests/null/ocaml/main.ml
@@ -0,0 +1,83 @@
+(*-*-Mode:ocaml;coding:utf-8;tab-width:2;c-basic-offset:2;indent-tabs-mode:()-*-
+  ex: set ft=ocaml fenc=utf-8 sts=2 ts=2 sw=2 et nomod: *)
+
+(*
+ 
+  BSD LICENSE
+  
+  Copyright (c) 2017, Michael Truog <mjtruog at gmail dot com>
+  All rights reserved.
+  
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+  
+      * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+      * Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in
+        the documentation and/or other materials provided with the
+        distribution.
+      * All advertising materials mentioning features or use of this
+        software must display the following acknowledgment:
+          This product includes software developed by Michael Truog
+      * The name of the author may not be used to endorse or promote
+        products derived from this software without specific prior
+        written permission
+  
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+  DAMAGE.
+ 
+ *)
+
+let request api
+  type_ name pattern request_info request timeout priority trans_id pid =
+  print_endline "null ocaml" ;
+  Cloudi.Null
+
+let task thread_index =
+  match Cloudi.api thread_index with
+  | Error (error) ->
+    prerr_endline error
+  | Ok (api) ->
+    match Cloudi.subscribe api "ocaml/get" request with
+    | Error (error) ->
+      prerr_endline error
+    | Ok _ ->
+      match Cloudi.poll api (-1) with
+      | Error (error) ->
+        prerr_endline error
+      | Ok (timeout) ->
+        print_endline "terminate null ocaml"
+
+let () = 
+  match Cloudi.thread_count () with
+  | Error (error) ->
+    prerr_endline error
+  | Ok (thread_count) ->
+    let rec loop thread_index threads =
+      if thread_index = thread_count then
+        threads
+      else
+        let thread = Thread.create task thread_index in
+        loop (thread_index + 1) ([thread] @ threads)
+    and wait = function
+      | [] ->
+        ()
+      | thread::threads ->
+        Thread.join thread ;
+        wait threads
+    in
+    wait (loop 0 [])
+
