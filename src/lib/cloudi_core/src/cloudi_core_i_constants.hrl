138fc6eb364683229950ff8b54a9ff47498d5024
Michael Truog <mjtruog@gmail.com>
2017-04-10 09:48:53 -0700

    Add Erlang map type specification usage.

--- src/lib/cloudi_core/src/cloudi_core_i_constants.hrl
+++ src/lib/cloudi_core/src/cloudi_core_i_constants.hrl
@@ -48,13 +48,11 @@
 %  due to the extra compilation required for external services support)
 %-define(CLOUDI_CORE_STANDALONE, true).
 
-% handle the dict type change
+% "proxy" types for compatibility checking
 -type dict_proxy(Key, Value) :: dict:dict(Key, Value).
-
-% handle the queue type change
 -type queue_proxy(Value) :: queue:queue(Value).
+-type maps_proxy(Key, Value) :: #{Key := Value}.
 
--type maps_proxy(_Key, _Value) :: any().
 -define(MAP_NEW(),           maps:new()).
 -define(MAP_FIND(K, M),      maps:find(K, M)).
 -define(MAP_FETCH(K, M),     maps:get(K, M)).
