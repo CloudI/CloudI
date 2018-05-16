6e0deac0c4dbdd8424a04371ea348966be368676
Michael Truog <mjtruog@protonmail.com>
2018-05-15 21:05:20 -0700

    Update typespecs for the CloudI Service API function services_status.

--- src/lib/cloudi_core/src/cloudi_service_api.erl
+++ src/lib/cloudi_core/src/cloudi_service_api.erl
@@ -552,13 +552,21 @@
     nonempty_list({count_process, pos_integer()} |
                   {uptime_total, nonempty_string()} |
                   {uptime_running, nonempty_string()} |
-                  {uptime_restarts, nonempty_string()}).
+                  {uptime_restarts, nonempty_string()} |
+                  {availability_day, nonempty_string()} |
+                  {availability_week, nonempty_string()} |
+                  {availability_month, nonempty_string()} |
+                  {availability_year, nonempty_string()}).
 -type service_status_external() ::
     nonempty_list({count_process, pos_integer()} |
                   {count_thread, pos_integer()} |
                   {uptime_total, nonempty_string()} |
                   {uptime_running, nonempty_string()} |
-                  {uptime_restarts, nonempty_string()}).
+                  {uptime_restarts, nonempty_string()} |
+                  {availability_day, nonempty_string()} |
+                  {availability_week, nonempty_string()} |
+                  {availability_month, nonempty_string()} |
+                  {availability_year, nonempty_string()}).
 -type service_status() ::
     service_status_internal() | service_status_external().
 -export_type([service_id/0,
