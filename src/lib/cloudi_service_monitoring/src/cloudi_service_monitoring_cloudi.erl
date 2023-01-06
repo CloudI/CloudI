ad284ee18b4340699a94d8a90a2bbc2b0cd7f448
Michael Truog <mjtruog@protonmail.com>
2023-01-05 20:17:22 -0800

    Update LICENSE.

--- src/lib/cloudi_service_monitoring/src/cloudi_service_monitoring_cloudi.erl
+++ src/lib/cloudi_service_monitoring/src/cloudi_service_monitoring_cloudi.erl
@@ -8,7 +8,7 @@
 %%%
 %%% MIT License
 %%%
-%%% Copyright (c) 2015-2022 Michael Truog <mjtruog at protonmail dot com>
+%%% Copyright (c) 2015-2023 Michael Truog <mjtruog at protonmail dot com>
 %%%
 %%% Permission is hereby granted, free of charge, to any person obtaining a
 %%% copy of this software and associated documentation files (the "Software"),
@@ -29,8 +29,8 @@
 %%% DEALINGS IN THE SOFTWARE.
 %%%
 %%% @author Michael Truog <mjtruog at protonmail dot com>
-%%% @copyright 2015-2022 Michael Truog
-%%% @version 2.0.5 {@date} {@time}
+%%% @copyright 2015-2023 Michael Truog
+%%% @version 2.0.6 {@date} {@time}
 %%%------------------------------------------------------------------------
 
 -module(cloudi_service_monitoring_cloudi).
